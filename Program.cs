b4f2e599b2fabe5aab806d2b69b742be7e5d5be6
Omar Zanaty <99298839+Blue22796@users.noreply.github.com>
2023-11-24 19:18:50 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,3 +20,5 @@ app.Run();
 
 //https://drive.google.com/u/0/uc?id=1QP7o3_uVKZOZP3D8NcTk1OxbWfRnC_zj&export=download
 //http://fs1.megadb.xyz:8080/d/7dnhxwaoy5vxygoid7w7rv2em3yqqnyw4eo3tu6v4lbcrm44iwidcxqfteoojqw4gywjozan/OCTOPATH-TRAVELER-SteamRIP.com.rar
+//https://1fichier.com/?abvb9rbyj380lmsf7q1w%20
+//https://megadb.net/download
