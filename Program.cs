cdb60665945226f7e15d86e51b175915a8dace8d
Omar Zanaty <99298839+Blue22796@users.noreply.github.com>
2023-12-07 13:37:26 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,8 +17,3 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.Run();
-
-//https://drive.google.com/file/d/1rHMfGWe1mUAv3XxJ_mIUoIaJTlU4Vn11/view?usp=sharing
-//https://drive.google.com/file/d/1wGMd2svGh2uGhV5BpnxhFMrycas1nA6z/view?usp=sharing
-//https://groups.google.com/g/itskayoanime/c/1-5fT7wPz58?pli=1
-//https://drive.google.com/drive/folders/1r2ld27sz_3VTnjzuvEavf0t2lddRucZO
